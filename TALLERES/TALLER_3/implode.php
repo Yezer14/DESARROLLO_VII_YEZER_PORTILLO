6dd860fe1ae71e4408e1ae660ac4e76c35d23d2c
Yezer <yezerxd1@hotmail.com>
2024-08-27 19:01:20 -0500

    arreglo del ejercicio implode()

--- TALLERES/TALLER_3/implode.php
+++ TALLERES/TALLER_3/implode.php
@@ -5,10 +5,10 @@ $frutas = ["Manzana", "Naranja", "Plátano", "Uva"];
 $frase = implode(", ", $frutas);
 
 echo "Array de frutas:
-";<br>
+";
 print_r($frutas);
 echo "Frase creada: $frase
-";<br>
+";
 
 // Ejercicio: Crea un array con los nombres de 5 países que te gustaría visitar
 // y usa implode() para convertirlo en una cadena separada por guiones (-)
@@ -17,7 +17,7 @@ $listaPaises = implode("-", $paises);
 
 echo "
 Mi lista de países para visitar: $listaPaises
-";<br>
+";
 
 // Bonus: Usa implode() con un array asociativo
 $persona = [
